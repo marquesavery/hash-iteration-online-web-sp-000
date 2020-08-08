82906bbd825cfd64d25b5bccb240f2faf2348996
Marques Avery <averymarques4@gmail.com>
2020-08-08 03:43:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,4 +12,6 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
 end
